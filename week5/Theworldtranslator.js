96615d99b3d6ed8954e2fb01d1d6295271740448
Yusuf ilhan <yusuf.ilhan@powercoders.org>
2022-10-17 16:51:58 +0200

    i added week5 exercises

---
+++ week5/Theworldtranslator.js
@@ -0,0 +1,12 @@
+function helloWorld(lang) {
+  if (lang == "fr") {
+    return "Bonjour, monde";
+  } else if (lang == "es") {
+    return "Hola, Mundo";
+  } else {
+    return "Hello, World";
+  }
+}
+alert(helloWorld("en"));
+alert(helloWorld("fr"));
+alert(helloWorld("es"));
