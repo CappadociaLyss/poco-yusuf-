c6032cd72be41f2059baf5ba9082f2ffb90eb204
Yusuf ilhan <yusuf.ilhan@powercoders.org>
2022-10-17 21:22:22 +0200

    small changes done

--- week5/bigger.js
+++ week5/bigger.js
@@ -1,15 +1,10 @@
-// var num1 = Number;
-// var num2 = Number;
-
 let num1 = prompt ("Please enter the first number");
 let num2 = prompt ("please enter the second number");
-
 
 if (isNaN (num1)||isNaN (num2)){
     alert("please enter number")
 }
 else {
-
     
     if (num1 >= num2){
     
@@ -17,8 +12,7 @@ else {
     }
     else ( num2 >= num1)
         bigger = num2;
-    
-        
+            
     alert("The larger number is " + bigger);
 
 }
