e84f82ca8770de5cdcb42f72ce588c34b9dc534d
Yusuf ilhan <yusuf.ilhan@powercoders.org>
2022-10-21 16:13:45 +0200

    I added exercises of Week 5

---
+++ week5/friday/ManipulatetheDOM.js
@@ -0,0 +1,18 @@
+let HH1 = document.getElementById("Hone");
+let p = document.querySelector(".new");
+let alink = document.querySelector(".W3school");
+
+for (let i = 0; i < 2; i++) {
+  //appending (ekleme)
+  HH1.textContent += i;
+  // HH1.textContent = HH1.textContent + " " + i;
+} 
+
+for (let i = 0; i < 4; i++) {
+  //prepending (başa eklemek)
+  p.textContent = i + " " + p.textContent;
+}
+
+for (let i = 0; i < 7; i++) {
+  //replacing (değiştirme)
+  alink.textContent = i;}
